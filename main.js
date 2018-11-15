c1bc336ab8d27343e9d2bf41002b61f189f210c6
jeremywp <dr.jpratt@gmail.com>
2018-11-15 10:06:47 -0700

    initial

--- main.js
+++ main.js
@@ -1,23 +1,50 @@
 
-let myArray = [1,3,5,9,17,21,82,96,100];
+let myArray = [1,3,5,9,17,21,82,96,100,130,170,191,200,201,222,257,299,357,375];
 
-let BinarySearch = function (arr, low, high, searchParam) {
+function BinarySearch (arr, data) {
 
-    if (high >= low) {
-        let midpoint = low + (high - low)/2;
+    let low = 0;
+    let high = arr.length - 1;
+    let midpoint = Math.ceil((low + high) / 2);
 
-        if (searchParam = arr[midpoint]) {
-           return midpoint;
+    while (arr[midpoint] !== data && low <= high) {
+
+        if (data < arr[midpoint]) {
+            high = midpoint - 1;
+            console.log("Binary searching...");
+        } else {
+            low = midpoint + 1;
+            console.log("Binary searching...");
         }
-        else if (searchParam < arr[midpoint]) {
-            return BinarySearch(arr, low, midpoint - 1, searchParam)
+
+        midpoint = Math.ceil((low + high) / 2);
+    }
+
+    return (arr[midpoint] !== data) ? console.log("Number not found") : console.log("Your number is at index: " + midpoint);
+}
+
+function linearSearch(arr, data)
+{
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === data) {
+            console.log("Number can be found at: " + i);
+            return i;
         }
-        else if (searchParam > arr[midpoint]) {
-            return BinarySearch(arr, midpoint + 1, high, searchParam)
+        else {
+            console.log("Linear searching...");
         }
     }
-};
+}
 
-let result = BinarySearch(myArray, 0 , 8, 17);
+let linearStartTime = performance.now();
+linearSearch(myArray, 375);
+let linearEndTime = performance.now();
+let lineSearchTime = linearEndTime - linearStartTime;
+console.log("Linear Search took: " + lineSearchTime + " milliseconds");
 
-console.log("Your number is at index: " + result);
+
+let binaryStartTime = performance.now();
+BinarySearch(myArray, 375);
+let binaryEndTime = performance.now();
+let binaSearchTime = binaryEndTime - binaryStartTime;
+console.log("Linear Search took: " + binaSearchTime + " milliseconds");
